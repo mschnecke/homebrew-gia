9ea5d83cac35104201715d1c5e10fd002d942c40
Michael Schnecle <mschnecke@pisum.net>
2025-11-02 16:54:22 +0100

    Add Formula for Gia application to Homebrew
    This commit introduces a Homebrew formula for the Gia application, supporting both Intel and ARM architectures. It includes installation steps, post-install adjustments, and guidance for resolving potential Gatekeeper warnings.

---
+++ Formula/gia.rb
@@ -0,0 +1,47 @@
+class Gia < Formula
+  desc "Beschreibung deiner Anwendung"
+  homepage "https://github.com/mschnecke/gia"
+  version "1.0.0"
+  license "MIT" # Passe die Lizenz an
+
+  if Hardware::CPU.intel?
+    url "https://github.com/mschnecke/gia/releases/download/v#{version}/gia-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "INTEL_SHA256_HIER" # Mit `shasum -a 256 datei.tar.gz` berechnen
+  else
+    url "https://github.com/mschnecke/gia/releases/download/v#{version}/gia-#{version}-aarch64-apple-darwin.tar.gz"
+    sha256 "ARM_SHA256_HIER"
+  end
+
+  def install
+    # Binaries installieren
+    bin.install "gia"
+    bin.install "giagui"
+  end
+
+  def post_install
+    # Ausführbar machen (normalerweise nicht nötig, da bin.install das macht)
+    chmod 0755, bin/"gia"
+    chmod 0755, bin/"giagui"
+    
+    # Aus Gatekeeper-Quarantäne entfernen
+    system "xattr", "-d", "com.apple.quarantine", bin/"gia" rescue nil
+    system "xattr", "-d", "com.apple.quarantine", bin/"giagui" rescue nil
+  end
+
+  def caveats
+    <<~EOS
+      Die Programme gia und giagui wurden installiert.
+      
+      Falls beim ersten Start eine Gatekeeper-Warnung erscheint,
+      kannst du die Programme manuell freigeben mit:
+        
+        sudo xattr -d com.apple.quarantine #{bin}/gia
+        sudo xattr -d com.apple.quarantine #{bin}/giagui
+    EOS
+  end
+
+  test do
+    # Einfacher Test, ob die Binaries ausführbar sind
+    assert_match version.to_s, shell_output("#{bin}/gia --version")
+  end
+end
